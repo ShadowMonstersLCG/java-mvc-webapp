9c58ab6e8fb5b03c48ad96a69481bed96b5de0a2
Hima <hima.poonadathu@lcginc.com>
2019-12-14 16:30:30 -0500

    Commit for Dashboard page change

--- src/main/java/com/lcg/controllers/HomeController.java
+++ src/main/java/com/lcg/controllers/HomeController.java
@@ -2,18 +2,34 @@ package com.lcg.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class HomeController {
 
 	public HomeController() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	@GetMapping("/shadow-monsters")
-	public String showHomePage(){
-		System.out.println("In home page");
+	public String showHomePage(){		
 		return "home";
+	}
+	
+	@PostMapping("/userAunthentication")
+	public String userAunthentication(@RequestParam("user") String user,@RequestParam("pass") String pass){
+		System.out.println("User : " +user + " Pass : " +pass);
+		RestTemplate restTemplate =new RestTemplate();
+		String isValidUser = restTemplate.getForObject("http://localhost:8081/userLogin/"+user+"/"+pass, String.class);		
+		System.out.println("isValidUser :" +isValidUser);
+		return "userDashboard";
+	}
+	
+	@PostMapping("/userRegistration")
+	public String userRegistration(){		
+		return "userRegistration";
 	}
 
 }
--- src/main/webapp/WEB-INF/jsp/home.jsp
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -22,13 +22,15 @@
     
 <div class="login-form">
 <div class="sign-in-htm">
+
+<form action="/userAunthentication" method="POST" >
 <div class="group">
 <label for="user" class="label">Username</label>
-<input id="user" type="text" class="input">
+<input id="user" name="user" type="text" class="input">
 </div>
 <div class="group">
 <label for="pass" class="label">Password</label>
-<input id="pass" type="password" class="input" data-type="password">
+<input id="pass" name="pass" type="password" class="input" data-type="password">
 </div>
 <div class="group">
 <input id="check" type="checkbox" class="check" checked>
@@ -37,6 +39,7 @@
 <div class="group">
 <input type="submit" class="button" value="Sign In">
 </div>
+
 <div class="hr"></div>
 <div class="foot-lnk">
 <a href="#forgot">Forgot Password?</a>
@@ -80,14 +83,12 @@
 </div>
 <div class="hr"></div>
 <div class="foot-lnk">
-<label for="tab-1">Already Member?</a>
+<label for="tab-1">Already Registered?</a>
 </div>
 </div>
 </div>
 </div>
 </div>
-
-
-
+</form>
 </body>
 </html>
